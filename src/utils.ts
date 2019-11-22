a2cbd4732db06ab15b91a23aeba871465f4a67e9
Robert Kiel <robert.kiel@validitylabs.org>
2019-11-22 16:53:58 +0100

    🔧 Uint8Array comparison

--- src/utils.ts
+++ src/utils.ts
@@ -19,3 +19,7 @@ export function getId(self: AccountId, counterparty: AccountId) {
     return new Hash(blake2b(new AccountIdTuple(counterparty, self).toU8a(), ID_HASH_KEY, BYTESIZE))
   }
 }
+
+export function compareArray(a: Uint8Array, b: Uint8Array) {
+  return a.length == b.length && a.every((value, index) => value == b[index])
+}

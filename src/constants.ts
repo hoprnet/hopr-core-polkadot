260c913b013606593bcf8ece548d099254e4b4ec
Robert Kiel <robert.kiel@validitylabs.org>
2020-03-10 11:36:50 +0100

    ♻︎ API upgrade

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 import { Constants as IConstants } from '@hoprnet/hopr-core-connector-interface'
 
 export default class implements IConstants {
-    HASH_LENGTH = 32
-    SIGNATURE_LENGTH = 160
+  HASH_LENGTH = 32
+  SIGNATURE_LENGTH = 160
+
+  NETWORK = `testnet`
 }

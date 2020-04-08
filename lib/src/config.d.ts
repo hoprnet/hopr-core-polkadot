06b2219136409a3e1fd30a838062b8c7cae87ec5
Constantidis <constantidis@protonmail.com>
2020-04-08 14:32:43 +0200

    add autobuild

--- lib/src/config.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const polkadotBasepath = "../../hopr-polkadot";
-export declare const DEFAULT_URI = "ws://localhost:9944";
-export declare const FUND_ACCOUNT_PRIVATE_KEY = 9.370184697826372e+76;
-/**
- * These private keys serve as demo identities for local testnets.
- *
- * @notice Do NOT use them to store any meaningful assets!
- */
-export declare const DEMO_ACCOUNTS: string[];
--- lib/src/constants.d.ts
+++
@@ -1,5 +0,0 @@
-import { Constants as IConstants } from '@hoprnet/hopr-core-connector-interface';
-export default class implements IConstants {
-    HASH_LENGTH: number;
-    SIGNATURE_LENGTH: number;
-}
